24a5bfd2fb82b81e4b1e16bd2cba9a1889711097
Eduardo Motton <emotton@gmail.com>
2022-02-13 23:11:50 -0300

    Atualizacao OK

--- Modulo 02/persistencia/02_TYPESCRIPT/src/controllers/ping.ts
+++ Modulo 02/persistencia/02_TYPESCRIPT/src/controllers/ping.ts
@@ -1,6 +1,22 @@
-import { Get, Route, Tags } from 'tsoa';
+import { Body, Get, Path, Post, Put, Route, SuccessResponse, Tags } from "tsoa";
 interface PingResponse {
   message: string;
+}
+
+interface Ping2Response {
+  message: string;
+  status: boolean;
+}
+
+interface Ping3Response {
+  message: string;
+  status: boolean;
+  code: number;
+}
+
+export interface PingRequest {
+  message: string;
+  code: number;
 }
 
 @Route("ping")
@@ -10,6 +26,33 @@ export default class PingController {
   public async getMessage(): Promise<PingResponse> {
     return {
       message: "pong",
+    };
+  }
+
+  @Get("/test/{id}")
+  public async getMessageById(@Path("id") ID: number): Promise<Ping2Response> {
+    return {
+      message: `pong-${ID}`,
+      status: true,
+    };
+  }
+
+  @SuccessResponse('201', 'Created')
+  @Post("/test")
+  public async postMessage(@Body() requestBody: PingRequest): Promise<Ping2Response> {
+    console.log(requestBody.code);
+    return {
+      message: `pong-${requestBody.code}`,
+      status: true,
+    };
+  }
+
+  @Put("/test/{id}")
+  public async putMessage(@Path("id") ID: number, @Body() requestBody: PingRequest): Promise<Ping3Response> {
+    return {
+      message: `pong-${ID}`,
+      status: true,
+      code: requestBody.code
     };
   }
 }
--- Modulo 02/persistencia/02_TYPESCRIPT/src/routes/index.ts
+++ Modulo 02/persistencia/02_TYPESCRIPT/src/routes/index.ts
@@ -1,11 +1,29 @@
-import express from "express";
-import PingController from "../controllers/ping";
+import express, { Request } from "express";
+import PingController, { PingRequest } from "../controllers/ping";
 
 const router = express.Router();
 
 router.get("/ping", async (_req, res) => {
   const controller = new PingController();
   const response = await controller.getMessage();
+  return res.send(response);
+});
+
+router.get("/ping/test/:id", async (req: Request<{ id: string}>, res) => {
+  const controller = new PingController();
+  const response = await controller.getMessageById(Number(req.params.id));
+  return res.send(response);
+});
+
+router.post("/ping/test", async (req: Request<PingRequest>, res) => {
+  const controller = new PingController();
+  const response = await controller.postMessage(req.body);
+  return res.status(201).send(response);
+});
+
+router.put("/ping/test/:id", async (req: Request<{ id: string}, PingRequest>, res) => {
+  const controller = new PingController();
+  const response = await controller.putMessage(Number(req.params.id), req.body);
   return res.send(response);
 });
 
