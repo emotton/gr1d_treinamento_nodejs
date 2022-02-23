a6a36880d0871043ab4fd22111d2dacacf1a9eb8
Eduardo Motton <emotton@gmail.com>
2022-02-23 14:16:39 -0300

    Inclusao Vue

---
+++ Modulo 03/01_VUE/vue-projeto/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Eduardo F Motton</h2>
+    <button @click="carregarClientes">Carregar</button>
+    <div>
+      <ul>
+        <li v-for="cliente in clientes" :key="cliente.id">{{cliente.nome}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      clientes: []
+    }
+  },
+  methods: {
+    carregarClientes() {
+      this.$http.get("http://localhost:3000/clientes").then((response) => {
+        this.clientes = response.data;
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
