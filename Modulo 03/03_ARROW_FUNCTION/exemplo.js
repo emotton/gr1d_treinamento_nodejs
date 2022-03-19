c1aa338fd9792490b1afa0b67cad58e320f87038
emotton <emotton@gmail.com>
2022-03-18 21:07:40 -0300

    Inclusão 03_ARROW_FUNCTION

---
+++ Modulo 03/03_ARROW_FUNCTION/exemplo.js
@@ -0,0 +1,38 @@
+var func1 = function (param1) {
+  return param * 2;
+};
+
+// Function Statement
+var func2 = (param1) => {
+  return param1 * 2;
+};
+
+// Expression Body
+var func3 = (param1) => param1 * 2;
+
+// Contexto
+
+// Function utilizando this está se referenciando ao contexto body da própria
+// Arrow Function this está se referenciando ao contexto externo (PAI)
+
+function Factory() {
+  this.contador = 0;
+  this.create = () => {
+    return {
+      nome: "Eduardo",
+      idade: 52,
+      contador: ++this.contador,
+    };
+  };
+}
+
+var factory = new Factory();
+
+var obj = factory.create();
+console.log(obj);
+
+var obj2 = factory.create();
+console.log(obj2);
+
+var obj3 = factory.create();
+console.log(obj3);
